f3fde2b9b1ae7d40e7335499495eff51c2ae6871
yeticheese <abumohd.zalrahman@gmail.com>
2023-09-12 17:38:09 +0600

    - Dataset Loading functions, requires refinement

--- src/process_functions.py
+++ src/process_functions.py
@@ -1,5 +1,7 @@
 from .utils import write_dict_to_hdf5
 import h5py
+import numpy as np
+import pandas as pd
 
 
 def dict_write(rem_dict, header_dict, write_filename):
@@ -8,3 +10,22 @@ def dict_write(rem_dict, header_dict, write_filename):
     file = h5py.File(write_filename, 'w')
     write_dict_to_hdf5(file, rem_record)
     file.close()
+
+
+def cog_dataset_loader(rem_dict):
+    cog = np.empty((0, 2))
+    for k, sub_dict in rem_dict.items():
+        if k == 'header_info':
+            continue
+        else:
+            for key, value in sub_dict.items():
+                if key == 'CoG':
+                    cog = np.vstack((cog, value))
+                else:
+                    continue
+    return cog
+
+
+def dataset_to_csv(cog, data_filename):
+    cog_df = pd.DataFrame(cog)
+    cog_df.to_csv(data_filename, mode='a', header=False, index=False)
